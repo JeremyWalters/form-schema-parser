d7b1c07dcf46417de34f571beb9361531bb278f6
Jeremy Walters <walterjc21@gmail.com>
2018-09-20 17:19:40 +0200

    Add index files

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { BaseComponent } from './baseComponent';
+export { DropDownComponent } from './dropdownComponent';
+export { CheckBoxComponent } from './checkboxComponent';
+export { TextFieldComponent } from './textfieldComponent';
